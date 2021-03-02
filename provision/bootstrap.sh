416143d06e00f2c164c5c1b8702f840fc715456d
rdrgporto <rdrgporto@gmail.com>
2021-03-02 21:20:36 +0100

    Update to Docker 19.03.15, Kubernetes 1.20.4 & Containerd 1.4.3-1 :sparkles:

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -8,8 +8,8 @@ DATE() {
 # Variables
 IP=`ip -o addr show up primary scope global | while read -r num dev fam addr rest; do echo [$(DATE)] [Info] [System] ${addr%/*}; done`
 VM_USER=vagrant
-DOCKER_VERSION=19.03.12
-KUBERNETES_VERSION=1.20.0
+DOCKER_VERSION=19.03.15
+KUBERNETES_VERSION=1.20.4
 
 # Non-Interactive Installation
 export DEBIAN_FRONTEND=noninteractive
@@ -43,7 +43,7 @@ add-apt-repository \
 
 # Install Docker
 apt -y update &> /dev/null
-apt -y install docker-ce=5:$DOCKER_VERSION~3-0~ubuntu-xenial docker-ce-cli=5:$DOCKER_VERSION~3-0~ubuntu-xenial containerd.io=1.2.13-1 &> /dev/null
+apt -y install docker-ce=5:$DOCKER_VERSION~3-0~ubuntu-bionic docker-ce-cli=5:$DOCKER_VERSION~3-0~ubuntu-bionic containerd.io=1.4.3-1 &> /dev/null
 
 # To run Docker without sudo
 usermod -aG docker $VM_USER &> /dev/null
